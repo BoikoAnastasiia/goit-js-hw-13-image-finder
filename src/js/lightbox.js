a4fc6794e8f68b00c86a740bc1745d033e0f6a5b
anastasyah86 <nastasyah86@gmail.com>
2021-02-15 19:22:39 +0200

    mess

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -3,18 +3,13 @@ import 'basicLightbox/src/styles/main.scss';
 import refs from './refs';
 
 const lightBox = () => {
-  refs.galleryContainer.addEventListener('click', event => {
-    const target = event.target;
-    // console.log(target);
-    // console.log(refs.pic);
-    if (img.nodeName !== 'IMG') {
-        return
-        {
-      basicLightbox.create(`
-    <img src="assets/images/image.png" width="1280" height="854">
-`);
-      lightBox.show();
+  refs.galleryContainer.addEventListener('click', () => {
+    if (img.nodeName == 'IMG') {
+      return;
     }
+
+    basicLightbox.create(`${IMG.dataset.source}`);
+    lightBox.show();
   });
 };
 
