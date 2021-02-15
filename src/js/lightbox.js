468ce307332ff9961787c076b8bf94f37439d6ee
anastasyah86 <nastasyah86@gmail.com>
2021-02-15 17:46:22 +0200

    lightnox is crooked

--- src/js/lightbox.js
+++ src/js/lightbox.js
@@ -3,19 +3,23 @@ import 'basicLightbox/src/styles/main.scss';
 import refs from './refs';
 
 const lightBox = () => {
-  basicLightbox.create(
-    `
-		<img width="1400" height="900" src="https://placehold.it/1400x900">
-	`,
-  );
+  
 
   refs.galleryContainer.addEventListener('click', event => {
     const target = event.target;
     console.log(target);
-    if (target === refs.pic) {
+    console.log(refs.pic);
+      if (target === refs.pic)
+      {
       lightBox.show();
     }
   });
+    
+    basicLightbox.create(
+    `
+		<img width="1400" height="900" src=`${refs.lightBox.src}`>
+	`,
+  );
 };
 
 export default lightBox;
